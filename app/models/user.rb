a73f34f1790a993eb37a444d825a27d32512507c
Estelle Boujenah <boujenahestelle@hotmail.fr>
2020-03-12 11:39:38 +0100

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :offers
   has_many :bookings
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
