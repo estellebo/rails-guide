d9efff8dfa0d2008b5096c4736321ad5ac5d06cb
Estelle Boujenah <boujenahestelle@hotmail.fr>
2020-03-12 11:17:19 +0100

    add search

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,12 @@
 class Offer < ApplicationRecord
   # geocoded_by :meeting_place
   # after_validation :geocode, if: :will_save_change_to_meeting_place?
+  include PgSearch::Model
+  pg_search_scope :search_by_city_and_meeting_place,
+    against: [ :city, :meeting_place ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 
   belongs_to :user
   has_one :booking
